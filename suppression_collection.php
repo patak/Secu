82923c0bc388a7f0826c70e36e78a26ef455970c
patak <pataky@hotmail.fr>
2015-10-07 15:13:25 +0200

    first commit

---
+++ suppression_collection.php
@@ -0,0 +1,25 @@
+<?php
+require('include/connexion.php');
+
+$bSupprimer = false;
+/**
+ * Vérifie si un identifiant de collection est fourni
+ * et si celui-ci est bien un entier
+ */
+$iIdentifiant = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+if (isset($_GET['id']) && false !== $iIdentifiant) :
+    /**
+     * Supprime les ouvrages de la collection
+     */
+    $sRequeteSql = 'DELETE FROM ouvrage WHERE collection_id = ' . $iIdentifiant;
+    $oConnexion->query($sRequeteSql);
+
+    /**
+     * Supprime la collection
+     */
+    $sRequeteSql = 'DELETE FROM collection WHERE id = ' . $iIdentifiant;
+    $oConnexion->query($sRequeteSql);
+    $bSupprimer = true;
+endif;
+
+header('Location: index.php?page=collection.php&etat_suppression=' . (int) $bSupprimer);
